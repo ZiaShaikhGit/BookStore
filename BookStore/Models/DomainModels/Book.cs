7d604963d1cf65941538e53423006265a30631a0
sziauddin <shaikh.ziauddin@dxc.com>
2021-04-25 22:58:39 +0530

    Add project files.

---
+++ BookStore/Models/DomainModels/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.DomainModels
+{
+	public class Book
+	{
+		public int BookId { get; set; }
+
+		[Required(ErrorMessage ="Please enter title")]
+		[MaxLength(200)]
+		public string Title { get; set; }
+
+		[Range(0.0,100000.0,ErrorMessage ="Price must be more than 0 and less than 1 Lakhs")]
+		public double Price { get; set; }
+
+		[Required(ErrorMessage ="Please select genre")]
+		public string GenreId { get; set; }
+
+		public Genre Genre { get; set; }
+
+		public ICollection<BookAuthor> BookAuthors { get; set; }
+	}
+}
